9062aaab3850db3487d8f57862269b54011c7bfa
madhuri4994 <madhuripatil506@gmail.com>
2018-09-10 10:22:20 +0530

    note created

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -5,43 +5,42 @@ var noteService = require('../services/NoteService');
 var styleSheet = require('../css/styles');
 var styles = styleSheet.style;
 export default class AddNote extends Component {
-    static navigationOptions = {
-        headerTitle:
-            <View style={{ flexDirection: 'row', marginLeft: 220}}>
-                <Icon name='fiber-pin' size={30} color="grey" iconStyle={{padding : 5}}/>
-                <Icon name='touch-app' size={30} color="grey" iconStyle={{padding : 5}}/>
-                <Icon name='archive' size={30} color="grey" iconStyle={{padding : 5}}/>
-            </View>,
-            header: () => ({
-                left: ( <Icon name={'chevron-left'} onPress={ () => { noteService.createNote() } }  /> ),  
-            }),
+    static navigationOptions = ({navigation}) =>{
+
+        const params = navigation.state.params || {noteTitle:"",noteDescription:""};
+        return {
+            headerLeft: (
+                <Icon name="arrow-back"
+                      onPress={() => {navigation.goBack();noteService.createNote(params)}}
+                      size={30} color="grey"
+                      iconStyle={{paddingLeft : 8}}/>
+             ),
+            headerTitle:
+                <View style={{ flexDirection: 'row', marginLeft: 220 }}>
+                    <Icon name='fiber-pin' size={30} color="grey" iconStyle={{ padding: 5 }} />
+                    <Icon name='touch-app' size={30} color="grey" iconStyle={{ padding: 5 }} />
+                    <Icon name='archive' size={30} color="grey" iconStyle={{ padding: 5 }} />
+                </View>
+        } 
     }
 
-   
-
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            title : '',
-            description : ''
+            title: '',
+            description: ''
         }
     }
-    componentDidMount(){
-        this.navigation.setParams({
-            title: this.state.title,
-            description : this.state.description
-           })
-    }
-  
+    
     render() {
         return (
             <View onPress={this.props.newComponent} style={{ position: 'relative', flexDirection: 'column', flex: 1 }}>
                 <View>
-                    <TextInput placeholder="Title" style={{ fontSize: 20, fontWeight: 'bold', padding: 15 }} 
-                     onChangeText={(title) => this.setState({ title })}/>
+                    <TextInput placeholder="Title" style={{ fontSize: 20, fontWeight: 'bold', padding: 15 }}
+                        onChangeText={(title) => this.props.navigation.setParams({noteTitle:title})} />
 
-                    <TextInput placeholder="Note" style={{ fontSize: 20, padding: 15 }} multiline={true} 
-                                  onChangeText={(description) => this.setState({ description })}/>
+                    <TextInput placeholder="Note" style={{ fontSize: 20, padding: 15 }} multiline={true}
+                        onChangeText={(description) => this.props.navigation.setParams({noteDescription:description})} />
 
                 </View>
 
--- src/services/NoteService.js
+++ src/services/NoteService.js
@@ -1,6 +1,18 @@
+import app from '../config/Firebase';
 
 module.exports ={
-    createNote : function (title,text){
-
+    createNote : function (note){
+        var database = app.database();
+        var userRef = database.ref('/notes');
+        userRef.push({
+            Notetitle : note.noteTitle,
+            NoteDesc : note.noteDescription,
+            isPin : false,
+            isTrash : false,
+            isArchive : false,
+            Reminder : '',
+            ImageUrl : '',
+            Labels : []
+        })
     }
 }
